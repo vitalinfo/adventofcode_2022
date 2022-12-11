220a943ab634b99bdb8dc25b8c266d028000af26
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2022-12-11 21:03:58 +0000

    Solution 11 start

---
+++ 11/solution.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'ostruct'
+require_relative '../base'
+
+class Solution < Base
+  require_relative 'solution/monkey'
+
+  private
+
+  attr_reader :monkeys
+
+  def parse_input
+    monkey = Solution::Monkey.new
+    input_lines.each do |line|
+      if line.strip.empty?
+        monkeys << monkey
+        monkey = Solution::Monkey.new
+      end
+
+      monkey.index = Regexp.last_match(1).to_i unless line.match(/Monkey (\d):/).nil?
+      unless line.match(/Starting items: (.+)/).nil?
+        monkey.items = Regexp.last_match(1).split(', ').map(&:to_i)
+      end
+      monkey.operation = Regexp.last_match(1) unless line.match(/Operation: new = (.+)/).nil?
+      monkey.divisible = Regexp.last_match(1).to_i unless line.match(/Test: divisible by (\d+)/).nil?
+      monkey.true_to = Regexp.last_match(1).to_i unless line.match(/If true: throw to monkey (\d)/).nil?
+      monkey.false_to = Regexp.last_match(1).to_i unless line.match(/If false: throw to monkey (\d)/).nil?
+    end
+
+    monkeys << monkey
+  end
+
+  def perform1
+    @monkeys = []
+    parse_input
+
+    20.times { perform_one(3) }
+
+    sorted = monkeys.sort_by { |monkey| -monkey.inspects_count }
+    sorted[0].inspects_count * sorted[1].inspects_count
+  end
+
+  def perform2
+    @monkeys = []
+    parse_input
+
+    1000.times { perform_one(1) }
+
+    p monkeys
+    # sorted = monkeys.sort_by { |monkey| -monkey.inspects_count }
+    # sorted[0].inspects_count * sorted[1].inspects_count
+  end
+
+  def perform_one(worry_level_divider)
+    monkeys.each do |monkey|
+      monkey.items.each do |item|
+        worry_level = monkey.worry_level_for(item) / worry_level_divider
+
+        divmod = worry_level.divmod(monkey.divisible)
+
+        index_to = divmod.last.zero? ? monkey.true_to : monkey.false_to
+
+        monkeys[index_to].items << (divmod.last.zero? ? monkey.divisible : (monkey.divisible + divmod.last))
+
+        monkey.inspects_count += 1
+      end
+      monkey.items = []
+    end
+  end
+end
---
+++ 11/solution/monkey.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Solution::Monkey
+  attr_accessor :inspects_count, :index, :items, :operation, :divisible, :true_to, :false_to
+
+  def initialize
+
+    @items = []
+    @inspects_count = 0
+  end
+
+  def append_item(item)
+    @items << item
+  end
+
+  def worry_level_for(item)
+    old = item
+
+    eval(operation)
+  end
+end
